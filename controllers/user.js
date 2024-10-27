6cdec1280405b4dead0b3d865869e62cce7efd8d
Juliya Lipatova <575@ukr.net>
2024-10-27 19:37:38 +0100

    controllers for register and login are added

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,12 @@
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
 import { User } from "../model/userModel.js";
 import { wrapperComponent } from "../helpers/cntrlWrapper.js";
 import { HttpError } from "../helpers/HttpError.js";
 
-const addNewUser = async (req, res) => {
+const {SECRET} = process.env
+
+const registration = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
@@ -14,6 +17,26 @@ const addNewUser = async (req, res) => {
   res.status(201).json({ name: newUser.name, email: newUser.email });
 };
 
+const login = async (req, res)=>{
+
+const {email, password} = await req.body;
+const user =await User.findOne({ email });
+if(!user){
+    throw HttpError(401, "E-mail or password is invalide")
+}
+const passwordCompare = bcrypt.compare(password, user.password);
+if (!passwordCompare){
+    throw HttpError(401, "E-mail or password is invalide")
+}
+const payload = {
+    id: user._id
+}
+const token = jwt.sign(payload, SECRET, {expiresIn: "12h"})
+res.json(token)
+}
+
+
 export default {
-  addNewUser: wrapperComponent(addNewUser),
+    registration: wrapperComponent(registration),
+    login: wrapperComponent(login)
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { validateBody } from '../../middelwares/validateBody.js';
 import schemas from '../../model/userModel.js';
-import controller from '../../controllers/user.js'
+import cntrl from '../../controllers/user.js'
 
 const authRouter = express.Router()
 
-authRouter.post("/register", validateBody(schemas.registerSchema), controller.addNewUser)
+authRouter.post("/register", validateBody(schemas.registerSchema), cntrl.registration)
+
+authRouter.post("/login", validateBody(schemas.loginSchema), cntrl.login)
 
 export default authRouter
