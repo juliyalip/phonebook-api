b165984e88a98c7b4126e6afde1a9fa446096822
Juliya Lipatova <575@ukr.net>
2025-02-18 16:16:20 +0100

    fix mistake

--- helpers/optimazedImage.js
+++ helpers/optimazedImage.js
@@ -1,20 +1,19 @@
 import sharp from "sharp";
 import path from 'path'
-import fs from 'fs/promises'
 
 export const optimazedImage = async (tempPath) => {
-    const optimazedPath = path.join(process.cwd(), "public", "avatars", `${Date.now()}.webp`);
-    await fs.mkdir(outputDir, { recursive: true });
-   
-    try {
-      await sharp(tempPath)
-        .resize({ width: 150, height: 150, fit: 'cover' }) 
-        .webp({ quality: 80 }) 
-        .toFile(optimazedPath);
-  
-      return optimazedPath; 
-    } catch (error) {
-      console.log(error);
-     }
-  };
+
+  const optimazedPath = path.join(process.cwd(), "public", "avatars", `${Date.now()}.webp`);
+
+  try {
+    await sharp(tempPath)
+      .resize({ width: 150, height: 150, fit: 'cover' })
+      .webp({ quality: 80 })
+      .toFile(optimazedPath);
+
+    return optimazedPath;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
